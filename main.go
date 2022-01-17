27eeca590de4f109f436c846378680e5b077ec26
Alistair Smith <hi@alistair.sh>
2022-01-17 21:31:06 +0000

    feat: change ifttt integration because ifttt sucks

--- main.go
+++ main.go
@@ -18,52 +18,39 @@ func main() {
 		DisableStartupMessage: true,
 	})
 
-	app.Post("/", func(c *fiber.Ctx) error {
-		payload := struct {
-			Status string `json:"status"`
-		}{}
+	app.Get("/connected", func(ctx *fiber.Ctx) error {
+		println("received connected")
 
-		if err := c.BodyParser(&payload); err != nil {
+		now := time.Now()
+
+		// Safe to call login every time as we check
+		// if we are already logged in
+		if err := client.Login("932730208820265011"); err != nil {
+			println("Could not login to boosted RPC")
 			return err
 		}
 
-		println("Received new update: " + payload.Status)
-
-		switch payload.Status {
-		case "connected":
-			now := time.Now()
-
-			// Safe to call login every time as we check
-			// if we are already logged in
-			if err := client.Login("932730208820265011"); err != nil {
-				println("Could not login to boosted RPC")
-				return err
-			}
-
-			if err := client.SetActivity(client.Activity{
-				State:      "Riding my Boosted Board v3",
-				Details:    "Out and about...",
-				LargeImage: "v3",
-				LargeText:  "Picture of the v3 board",
-				SmallImage: "logo_black",
-				SmallText:  "Boosted Boards Logo",
-				Timestamps: &client.Timestamps{Start: &now},
-			}); err != nil {
-				println("Couldn't start boosted rpc! " + err.Error())
-				return err
-			}
-
-			break
-
-		case "disconnected":
-			client.Logout()
-			break
-
-		default:
-			println("unknown case received " + payload.Status)
+		if err := client.SetActivity(client.Activity{
+			State:      "Riding my Boosted Board v3",
+			Details:    "Out and about...",
+			LargeImage: "v3",
+			LargeText:  "Picture of the v3 board",
+			SmallImage: "logo_black",
+			SmallText:  "Boosted Boards Logo",
+			Timestamps: &client.Timestamps{Start: &now},
+		}); err != nil {
+			println("Couldn't start boosted rpc! " + err.Error())
+			return err
 		}
 
-		return c.SendStatus(200)
+		return ctx.SendStatus(200)
+	})
+
+	app.Get("/disconnected", func(ctx *fiber.Ctx) error {
+		println("received disconnected")
+
+		client.Logout()
+		return ctx.SendStatus(200)
 	})
 
 	println("ight we lit on " + port)
